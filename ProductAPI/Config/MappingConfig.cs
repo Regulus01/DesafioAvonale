3533045f48676d3159140dcbdadb4d824db26842
Jose <j.sousa.jr@hotmail.com>
2022-05-16 12:54:51 -0300

    Configurando AutoMapper

---
+++ ProductAPI/Config/MappingConfig.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using ProductAPI.Data;
+using ProductAPI.Models;
+
+namespace ProductAPI.Config
+{
+    public class MappingConfig
+    {
+
+        public static MapperConfiguration RegisterMaps()
+        {
+            var MappingConfig = new MapperConfiguration(Config =>
+            {
+                Config.CreateMap<ProductVO, Product>();
+                Config.CreateMap<Product, ProductVO>();
+            });
+            return MappingConfig;
+        }
+
+    }
+}
