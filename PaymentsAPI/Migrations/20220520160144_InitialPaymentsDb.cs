ee6221a27a61d3964526341ce689f09690b4ee13
Jose <j.sousa.jr@hotmail.com>
2022-05-20 13:02:22 -0300

    Cria migration em PaymentsAPI

---
+++ PaymentsAPI/Migrations/20220520160144_InitialPaymentsDb.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PaymentsAPI.Migrations
+{
+    public partial class InitialPaymentsDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Payments",
+                columns: table => new
+                {
+                    id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Value = table.Column<double>(nullable: false),
+                    Status = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payments", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Payments");
+        }
+    }
+}
--- PaymentsAPI/Model/Base/BaseEntity.cs
+++ PaymentsAPI/Model/Base/BaseEntity.cs
@@ -1,6 +1,12 @@
-﻿namespace PaymentsAPI.Model.Base
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PaymentsAPI.Model.Base
 {
     public class BaseEntity
     {
+        [Key]
+        [Column("id")]
+        public long Id { get; set; }
     }
 }
