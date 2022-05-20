781a27caac1df898e9ab2da4dce8798b9f6166db
Jose <j.sousa.jr@hotmail.com>
2022-05-20 12:41:16 -0300

    Cria Repository em PaymentsAPI

---
+++ PaymentsAPI/Repository/IPaymentsRepository.cs
@@ -0,0 +1,10 @@
+﻿using PaymentsAPI.Data;
+using System.Threading.Tasks;
+
+namespace PaymentsAPI.Repository
+{
+    public interface IPaymentsRepository
+    {
+        Task<PaymentsVO> Create (PaymentsVO vo);
+    }
+}
---
+++ PaymentsAPI/Repository/PaymentsRepository.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using PaymentsAPI.Data;
+using PaymentsAPI.Model;
+using PaymentsAPI.Model.Context;
+using System.Threading.Tasks;
+
+namespace PaymentsAPI.Repository
+{
+    public class PaymentsRepository : IPaymentsRepository
+    {
+        private readonly SqlServerContext _context;
+        private IMapper _mapper;
+
+        public PaymentsRepository(SqlServerContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<PaymentsVO> Create(PaymentsVO vo)
+        {
+            Payments payments = _mapper.Map<Payments>(vo);
+            _context.Payments.Add(payments);
+            await _context.SaveChangesAsync();
+            return _mapper.Map<PaymentsVO>(payments);
+        }
+    }
+}
