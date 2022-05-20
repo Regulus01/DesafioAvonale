fcb8e3dee2cd59545ec6d10f0f92a9773da11152
Jose <j.sousa.jr@hotmail.com>
2022-05-20 12:47:44 -0300

    Cria PaymentsController

---
+++ PaymentsAPI/Controllers/PaymentsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PaymentsAPI.Data;
+using PaymentsAPI.Repository;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Threading.Tasks;
+
+namespace PaymentsAPI.Controllers
+{
+    [Route("api/product/payments/purchase")]
+    [ApiController]
+    public class PaymentsController : Controller
+    {
+        private IPaymentsRepository _repository;
+
+        public PaymentsController(IPaymentsRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentException(nameof(repository));
+        }
+
+        [HttpPost]
+        [SwaggerOperation(Summary = "Create Payment")]
+        public async Task<ActionResult<PaymentsVO>> Create([FromBody] PaymentsVO vo)
+        {
+            if (vo == null)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                var product = await _repository.Create(vo);
+                return Ok(product);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
