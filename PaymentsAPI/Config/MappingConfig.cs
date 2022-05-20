85ef44285d99d97aabc5b9ceb2ead2cdccc005ea
Jose <j.sousa.jr@hotmail.com>
2022-05-20 12:23:48 -0300

    Configura AutoMapper

---
+++ PaymentsAPI/Config/MappingConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using PaymentsAPI.Data;
+using PaymentsAPI.Model;
+
+namespace PaymentsAPI.Config
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var MappingConfig = new MapperConfiguration(Config =>
+            {
+                Config.CreateMap<PaymentsVO, Payments>();
+                Config.CreateMap<Payments, PaymentsVO>();
+            });
+            return MappingConfig;
+        }
+    }
+}
