590857d691a7b609656a886030d77df755d032fa
Jose <j.sousa.jr@hotmail.com>
2022-05-20 12:21:24 -0300

    Cria PaymentsVO

---
+++ PaymentsAPI/Data/PaymentsVO.cs
@@ -0,0 +1,10 @@
+﻿using PaymentsAPI.Model.Enums;
+
+namespace PaymentsAPI.Data
+{
+    public class PaymentsVO
+    {
+        public double Value { get; set; }
+        public OrderStatus Status { get; set; }
+    }
+}
