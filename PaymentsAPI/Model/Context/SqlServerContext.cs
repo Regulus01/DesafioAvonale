ff938435d2fced8b85494f6540c121f2ff8b5b80
Jose <j.sousa.jr@hotmail.com>
2022-05-20 12:27:24 -0300

    Configura conexao com o PaymentsDb

---
+++ PaymentsAPI/Model/Context/SqlServerContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PaymentsAPI.Model.Context
+{
+    public class SqlServerContext : DbContext
+    {
+
+        protected SqlServerContext()
+        {
+
+        }
+
+        public SqlServerContext(DbContextOptions<SqlServerContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Payments> Payments { get; set; }
+    }
+}
+
